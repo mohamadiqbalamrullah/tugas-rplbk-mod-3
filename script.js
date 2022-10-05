fc9f4fc296803d1685e565da97432011bd25d8cc
Timothy <63138796+timothybagas@users.noreply.github.com>
2022-10-05 10:35:49 +0700

    Add handleOnClick

--- script.js
+++ script.js
@@ -1346,3 +1346,19 @@ const buildTable = (student) => {
 
     return wrapper;
 }
+
+const handleOnClick = () => {
+    const searchValue = document.getElementById('search').value;
+    
+    const desiredData = entireData.filter((student) => {
+        return student.nama_lengkap.toLowerCase().indexOf(searchValue) !== -1;
+    });
+
+    // reset element
+    const listOfStudents = document.getElementById('list-of-students');
+    listOfStudents.innerHTML = desiredData.length > 0 ? '' : 'Data Tidak Ditemukan!';
+
+    for (const student of desiredData) {
+        listOfStudents.append(buildTable(student));
+    }
+}

2854df31db976bde1cdea0846a5d713b5eaf1d92
mohamadiqbalamrullah <miqbalboy19@gmail.com>
2022-10-28 00:03:08 +0700

    add keterangan komponen

--- kULIAH/MATKUL/Semester 7/Praktikum RPLBK/Modul 3/tugasmod3/src/App.js
+++ kULIAH/MATKUL/Semester 7/Praktikum RPLBK/Modul 3/tugasmod3/src/App.js
@@ -39,8 +39,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>ini ceritanya screen time</p>
         <h1>SCREEN TIME</h1>
-        <Timer start="1" />
+        <Timer start="1"/>
+        Kalo ini ceritanya warna buttonnya ganti klo dipencet
         <Colors/>
       </header>
     </div>
